b1d79f20a832e2d351c0db08d4bfdda5e9a53a6b
Edelweiss603111 <arinavaruhina@gmail.com>
2018-06-12 14:11:35 +0500

    +

---
+++ SharedProject1/Class3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    class Class3
+    {
+        public Class3()
+        {
+            return;
+        }
+    }
+}
