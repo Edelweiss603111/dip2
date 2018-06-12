ccb1400a656f2dc79c2a25a3f57272f4e52c8ec9
Edelweiss603111 <arinavaruhina@gmail.com>
2018-06-12 16:04:34 +0500

    +

---
+++ ConsoleDip/ConsoleDip/ClassDip1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDip
+{
+    public class ClassDip1
+    {
+        private string _someName;
+
+        public ClassDip1()
+        {
+            _someName = "Arina";
+        }
+
+        public ClassDip1(string name)
+        {
+            _someName = name;
+        }
+
+        public string Message
+        {
+            get { return "Hello " + _someName; }
+            set { _someName = value; }
+        }
+    }
+}
---
+++ ConsoleDip/UnitTestDip/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleDip;
+
+namespace UnitTestDip
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClassDip1 hello1 = new ClassDip1("Sveta");
+            Assert.AreEqual("Hello Sveta", hello1.Message);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            ClassDip1 hello1 = new ClassDip1("Sega");
+            Assert.AreEqual("Hello Sveta", hello1.Message);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+    }
+}
