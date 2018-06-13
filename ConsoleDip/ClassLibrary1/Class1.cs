6d3c52f390e84a33a70968b9464183bf4e3e36ca
Edelweiss603111 <arinavaruhina@gmail.com>
2018-06-14 01:46:31 +0500

    +

---
+++ ConsoleDip/ClassLibrary1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ConsoleDip;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1
+    {
+        [TestCase]
+        public static void Test1()
+        {
+            ClassDip1 hello1 = new ClassDip1("World");
+            Assert.AreEqual("Hello Sveta", hello1.Message);
+        }
+
+        [TestCase]
+        public static void Test2()
+        {
+            ClassDip1 hello1 = new ClassDip1("World");
+            Assert.AreEqual("Hello World", hello1.Message);
+        }
+    }
+}
---
+++ ConsoleDip/UnitTestDip/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleDip;
+
+namespace UnitTestDip
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClassDip1 hello1 = new ClassDip1("World");
+            Assert.AreEqual("Hello Sveta", hello1.Message);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+    }
+}
